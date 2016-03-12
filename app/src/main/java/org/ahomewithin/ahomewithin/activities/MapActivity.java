972aa473b291732c05245302772b473b5852aa1e
Barbara Raitz <barbara.raitz@airbnb.com>
2016-03-12 09:51:22 -0800

    simple map activity

---
+++ app/src/main/java/org/ahomewithin/ahomewithin/activities/MapActivity.java
@@ -0,0 +1,44 @@
+package org.ahomewithin.ahomewithin.activities;
+
+/**
+ * Created by barbara on 3/12/16.
+ */
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.ahomewithin.ahomewithin.R;
+
+public class MapActivity extends MainActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FrameLayout flContainer = (FrameLayout) findViewById(R.id.flContent);
+        View v = getLayoutInflater().inflate(R.layout.content_map, null);
+        flContainer.addView(v);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we
+     * just add a marker near Africa.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
