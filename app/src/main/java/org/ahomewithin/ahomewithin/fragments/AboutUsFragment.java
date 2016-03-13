cc6d5e2d06243f2cc1085bb95a821d4c8f8b86e9
Barbara Raitz <barbara.raitz@airbnb.com>
2016-03-13 16:16:27 -0700

    fragment titles

--- app/src/main/java/org/ahomewithin/ahomewithin/fragments/AboutUsFragment.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/fragments/AboutUsFragment.java
@@ -3,16 +3,21 @@ package org.ahomewithin.ahomewithin.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.ahomewithin.ahomewithin.R;
 
+import butterknife.Bind;
+
 /**
  * Created by barbara on 3/13/16.
  */
 public class AboutUsFragment extends Fragment {
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     public static AboutUsFragment newInstance() {
         AboutUsFragment aboutUsFragment = new AboutUsFragment();
@@ -23,7 +28,7 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View convertView = inflater.inflate(R.layout.content_about_us, container, false);
-
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.about_us);
         return convertView;
     }
 
--- app/src/main/java/org/ahomewithin/ahomewithin/fragments/LearnMoreFragment.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/fragments/LearnMoreFragment.java
@@ -3,6 +3,7 @@ package org.ahomewithin.ahomewithin.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,7 @@ public class LearnMoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View convertView = inflater.inflate(R.layout.content_learn_more, container, false);
-
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.recommended);
         if (savedInstanceState == null) {
             RecommendedFragment fragmentRecommendations = RecommendedFragment.newInstance();
             getFragmentManager().beginTransaction()
