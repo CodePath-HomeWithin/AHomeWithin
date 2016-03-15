2ee16468ce7e59e46c06dfb200037065b876161c
Chezlui <joseluismartin@gmail.com>
2016-03-14 17:47:40 -0700

    temp

--- app/src/main/java/org/ahomewithin/ahomewithin/parseModel/ParseItem.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/parseModel/ParseItem.java
@@ -4,6 +4,7 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
 import org.ahomewithin.ahomewithin.models.Item;
+import org.json.JSONObject;
 
 import java.io.Serializable;
 
@@ -12,98 +13,108 @@ import java.io.Serializable;
  */
 @ParseClassName("Item")
 public class ParseItem extends ParseObject
-    implements Serializable {
+        implements Serializable {
 
-  public static final String PARSE_NAME = "Item";
-  //0 for video and 1 for card
-  //int type;
-  public static final String TYPE_KEY = "type";
+    public static final String PARSE_NAME = "Item";
+    //0 for video and 1 for card
+    //int type;
+    public static final String TYPE_KEY = "type";
 
-  //String title;
-  public static final String TITLE_KEY = "title";
+    //String title;
+    public static final String TITLE_KEY = "title";
 
-  //String desp;
-  public static final String DESP_KEY = "desp";
+    //String desp;
+    public static final String DESP_KEY = "desp";
 
-  //double price;
-  public static final String PRICE_KEY = "price";
+    //double price;
+    public static final String PRICE_KEY = "price";
 
-  //String imageUrl;
-  public static final String IMAGE_KEY = "image_url";
+    //String imageUrl;
+    public static final String IMAGE_KEY = "image_url";
 
-  //only available for video resource
-  //JSONObject content;
-  public static final String CONTENT_KEY = "content";
+    //only available for video resource
+    //JSONObject content;
+    public static final String CONTENT_KEY = "content";
 
 
-  public ParseItem() {
-    super(PARSE_NAME);
-  }
-
-  public Item.ITEM_TYPE getType() {
-    switch (getInt(TYPE_KEY)) {
-      default:
-      case 0: return Item.ITEM_TYPE.VIDEOS;
-      case 1: return Item.ITEM_TYPE.CONVERSATIONS;
-    }
-  }
-
-  public String getTitle() {
-    return getString(TITLE_KEY);
-  }
-
-  public String getDesp() {
-    return getString(DESP_KEY);
-  }
-
-  public double getPrice() {
-    return getDouble(PRICE_KEY);
-  }
-
-  public String getImage() {
-    return getString(IMAGE_KEY);
-  }
-
-  public String getContent() {
-    return getString(CONTENT_KEY);
-  }
-
-  public void setType(Item.ITEM_TYPE type) {
-    switch (type) {
-      default:
-      case VIDEOS: put(TYPE_KEY, 0); break;
-      case CONVERSATIONS: put(TYPE_KEY, 1); break;
+    public ParseItem() {
+        super(PARSE_NAME);
     }
 
-  }
+    public Item.ITEM_TYPE getType() {
+        switch (getInt(TYPE_KEY)) {
+            default:
+            case 0:
+                return Item.ITEM_TYPE.VIDEOS;
+            case 1:
+                return Item.ITEM_TYPE.CONVERSATIONS;
+        }
+    }
 
-  public void setTitle(String title) {
-    put(TITLE_KEY, title);
-  }
+    public String getTitle() {
+        return getString(TITLE_KEY);
+    }
 
-  public void setDesp(String desp) {
-    put(DESP_KEY, desp);
-  }
+    public String getDesp() {
+        return getString(DESP_KEY);
+    }
 
-  public void setPrice(double price) {
-    put(PRICE_KEY, price);
-  }
+    public double getPrice() {
+        return getDouble(PRICE_KEY);
+    }
 
-  public void setImage(String imageUrl) {
-    put(IMAGE_KEY, imageUrl);
-  }
+    public String getImage() {
+        return getString(IMAGE_KEY);
+    }
 
-  public void setContent(String content) {
-    put(CONTENT_KEY, content);
-  }
+    public String getContent() {
+        return getString(CONTENT_KEY);
+    }
 
-  public void update(Item item) {
-    setType(item.type);
-    setTitle(item.title);
-    setDesp(item.description);
-    setPrice(item.price);
-    setImage(item.imageUrl);
-    String content = (item.type == Item.ITEM_TYPE.VIDEOS) ? item.contentUrl : item.content;
-    setContent(content);
-  }
+    public JSONObject getContentJson() {
+        return getJSONObject(CONTENT_KEY);
+    }
+
+    public void setType(Item.ITEM_TYPE type) {
+        switch (type) {
+            default:
+            case VIDEOS:
+                put(TYPE_KEY, 0);
+                break;
+            case CONVERSATIONS:
+                put(TYPE_KEY, 1);
+                break;
+        }
+
+    }
+
+    public void setTitle(String title) {
+        put(TITLE_KEY, title);
+    }
+
+    public void setDesp(String desp) {
+        put(DESP_KEY, desp);
+    }
+
+    public void setPrice(double price) {
+        put(PRICE_KEY, price);
+    }
+
+    public void setImage(String imageUrl) {
+        put(IMAGE_KEY, imageUrl);
+    }
+
+    public void setContent(String content) {
+        put(CONTENT_KEY, content);
+    }
+
+    public void update(Item item) {
+        setType(item.type);
+        setTitle(item.title);
+        setDesp(item.description);
+        setPrice(item.price);
+        setImage(item.imageUrl);
+        String content = (item.type == Item.ITEM_TYPE.VIDEOS) ? item.contentUrl : item.content;
+        setContent(content);
+    }
 }
--- app/src/main/java/org/ahomewithin/ahomewithin/util/CardContentContent.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/util/CardContentContent.java
@@ -4,6 +4,10 @@ package org.ahomewithin.ahomewithin.util;
  * Created by xiangyang_xiao on 3/13/16.
  */
 public class CardContentContent{
+  public static final int MIND = 0;
+  public static final int BODY = 1;
+  public static final int HEART = 2;
+  public static final int SOUL = 3;
 
   public static CardContentContent getDefault() {
     return new CardContentContent("quote", "quote_authoer", "reflection", "action");
@@ -41,12 +45,21 @@ public class CardContentContent{
     this.reflection = reflection;
   }
 
+  public int type;
   private String quote;
   private String quote_author;
   private String reflection;
   private String action;
 
   public CardContentContent(String quote, String quote_author, String reflection, String action) {
+    this.quote = quote;
+    this.quote_author = quote_author;
+    this.reflection = reflection;
+    this.action = action;
+  }
+
+  public CardContentContent(int type, String quote, String quote_author, String reflection, String action) {
+    this.type = type;
     this.quote = quote;
     this.quote_author = quote_author;
     this.reflection = reflection;
