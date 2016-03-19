be3c150fc4112b93f60906cc5cd8937bbd95934d
Chezlui <joseluismartin@gmail.com>
2016-03-18 23:02:30 -0700

    About us section

--- app/src/main/java/org/ahomewithin/ahomewithin/fragments/AboutUsFragment.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/fragments/AboutUsFragment.java
@@ -1,23 +1,34 @@
 package org.ahomewithin.ahomewithin.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
 import org.ahomewithin.ahomewithin.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by barbara on 3/13/16.
  */
+// TODO this animation will be nice if time https://guides.codepath.com/android/Circular-Reveal-Animation
 public class AboutUsFragment extends Fragment {
-    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.ivAboutUs1) ImageView ivAboutUs1;
+    @Bind(R.id.ivAboutUs2) ImageView ivAboutUs2;
+    @Bind(R.id.ivAboutUs3) ImageView ivAboutUs3;
 
     public static AboutUsFragment newInstance() {
         AboutUsFragment aboutUsFragment = new AboutUsFragment();
@@ -28,8 +39,61 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View convertView = inflater.inflate(R.layout.content_about_us, container, false);
+        ButterKnife.bind(this, convertView);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.about_us);
         return convertView;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        int size = metrics.widthPixels/2;
+
+        Glide.with(getActivity())
+                .load(R.drawable.aboutus_1)
+                .asBitmap()
+                .centerCrop()
+                .override(size, size)
+                .into(new BitmapImageViewTarget(ivAboutUs1) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(getActivity().getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                ivAboutUs1.setImageDrawable(circularBitmapDrawable);
+            }
+        });
+
+        Glide.with(getActivity())
+                .load(R.drawable.aboutus_2)
+                .asBitmap()
+                .centerCrop()
+                .override(size, size)
+                .into(new BitmapImageViewTarget(ivAboutUs2) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(getActivity().getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        ivAboutUs2.setImageDrawable(circularBitmapDrawable);
+                    }
+                });
+
+        Glide.with(getActivity())
+                .load(R.drawable.aboutus_3)
+                .asBitmap()
+                .centerCrop()
+                .override(size, size)
+                .into(new BitmapImageViewTarget(ivAboutUs3) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(getActivity().getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        ivAboutUs3.setImageDrawable(circularBitmapDrawable);
+                    }
+                });
+    }
 }
