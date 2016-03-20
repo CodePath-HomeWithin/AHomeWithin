63289d1b338c19593e7404be820a2e04c11f6157
Barbara Raitz <barbara.raitz@airbnb.com>
2016-03-20 11:40:57 -0700

    more event detail

--- app/src/main/java/org/ahomewithin/ahomewithin/fragments/EventDetailFragment.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/fragments/EventDetailFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -15,6 +16,8 @@ import org.ahomewithin.ahomewithin.R;
 import org.ahomewithin.ahomewithin.models.Event;
 import org.ahomewithin.ahomewithin.models.Item;
 import org.ahomewithin.ahomewithin.util.UserTools;
+import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,12 +28,18 @@ import butterknife.ButterKnife;
 public class EventDetailFragment extends Fragment {
     Event mEvent;
 
+    @Bind(R.id.tvGroupName) TextView tvGroupName;
+    @Bind(R.id.tvEventName) TextView tvEventName;
+    @Bind(R.id.tvEventDescription) TextView tvEventDescription;
+    @Bind(R.id.tvDateTime) TextView tvDateTime;
+    @Bind(R.id.ivImage) ImageView ivImage;
+
     public static final String LOG_TAG = DetailFragment.class.getSimpleName();
 
 
     public static EventDetailFragment newInstance(Event event) {
         Bundle args = new Bundle();
-//        args.putParcelable(event);
+        args.putParcelable(Event.SERIALIZABLE_TAG, Parcels.wrap(event));
         EventDetailFragment fragment = new EventDetailFragment();
         fragment.setArguments(args);
         return fragment;
@@ -41,34 +50,17 @@ public class EventDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View convertView = inflater.inflate(R.layout.fragment_event_detail, container, false);
         ButterKnife.bind(this, convertView);
-//        mItem = (Event) getArguments().getSerializable(Event.SERIALIZABLE_TAG);
+        mEvent = (Event) Parcels.unwrap(getArguments().getParcelable(Event.SERIALIZABLE_TAG));
         return convertView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-//        Glide.with(getActivity()).load(mItem.imageUrl).into(ivItemImage);
-//        tvTitle.setText(mItem.title);
-//        tvDescription.setText(mItem.description);
-//
-//        // Calculate again to force update
-//        mItem.owned = UserTools.isItemPurchased(getActivity(), mItem.id);
-//        if (mItem.owned) {
-//            btBuy.setText("WATCH NOW");
-//        } else {
-//            btBuy.setText("BUY NOW");
-//        }
-//
-//        btBuy.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (mItem.owned) {
-//                    onWatch();
-//                } else {
-//                    onBuy();
-//                }
-//            }
-//        });
+        Glide.with(getActivity()).load(mEvent.imageUrl).into(ivImage);
+        tvGroupName.setText(mEvent.groupName);
+        tvEventName.setText(mEvent.eventName);
+        tvEventDescription.setText(mEvent.eventDescription);
+        tvDateTime.setText(mEvent.getDateTime());
     }
 
 }
