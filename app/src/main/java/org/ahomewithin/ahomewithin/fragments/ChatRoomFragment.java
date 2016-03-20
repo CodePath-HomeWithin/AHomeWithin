caa4fff9b7fa7a192997e2581409ac827cab3df0
Xiangyang_Xiao <Xiangyang.Xiao@airbnb.com>
2016-03-20 13:21:31 -0700

    [ahomewithin] Clear up some code

--- app/src/main/java/org/ahomewithin/ahomewithin/fragments/ChatRoomFragment.java
+++ app/src/main/java/org/ahomewithin/ahomewithin/fragments/ChatRoomFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -45,6 +46,7 @@ public class ChatRoomFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View chatRoomView = inflater.inflate(R.layout.fragment_chatroom, container, false);
         ButterKnife.bind(this, chatRoomView);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.chatRoom);
         return chatRoomView;
     }
 
